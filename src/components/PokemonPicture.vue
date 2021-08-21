66360e5480d72349004c0616f80b263db0f56cbe
steeltx <realsteel.gnu@gmail.com>
2021-08-20 23:53:42 -0500

    Diseño de componentes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,13 +1,40 @@
 <template>
-  <h3>Pokemon picture</h3>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="hidden-pokemon"
+      alt="pokemon"
+    />
+    <!-- <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="fade-in"
+      alt="pokemon"
+    /> -->
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+    
+};
 </script>
 
-<style>
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 
 </style>
