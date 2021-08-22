6b88376ced2d7c971ed10132f3487ba11f7dd4ef
steeltx <realsteel.gnu@gmail.com>
2021-08-21 23:02:09 -0500

    Funciones obtener nombres de pokemons

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,3 +1,4 @@
+import pokemonApi from '../api/pokemonApi'
 
 const getPokemons = () => {
 
@@ -9,19 +10,39 @@ const getPokemons = () => {
 
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
 
     // ordenar de forma aleatoria los elementos
     const mixedPokemons = getPokemons()
         .sort(() => Math.random() - 0.5)
     
     // pasar solo los primeros 4 a la funcion
-    getPokemonNames(mixedPokemons.splice(0,4))
+    const pokemons = await getPokemonNames(mixedPokemons.splice(0,4))
+
+    return pokemons
 
 }
 
-const getPokemonNames = ([ a,b,c,d] = []) => {
-    console.log(a,b,c,d)
+const getPokemonNames = async ([ a,b,c,d] = []) => {
+
+    // generación del arreglo con las 4 peticiones
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`)
+    ]
+
+    // realizar todas las peticiones
+    const [p1, p2, p3, p4] = await Promise.all(promiseArr)
+    
+    // generar objeto de regreso
+    return [
+        {name: p1.data.name, id: p1.data.id},
+        {name: p2.data.name, id: p2.data.id},
+        {name: p3.data.name, id: p3.data.id},
+        {name: p4.data.name, id: p4.data.id}
+    ]
 }
 
 export default getPokemonOptions
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -14,6 +14,7 @@ import PokemonOptions from '@/components/PokemonOptions.vue'
 import PokemonPicture from '@/components/PokemonPicture.vue'
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
+// console.log(getPokemonOptions())
 
 export default {
     components:{
