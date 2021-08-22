7bf86f5408837a2dc8bbf837c66fc9089ef2be37
steeltx <realsteel.gnu@gmail.com>
2021-08-22 18:27:57 -0500

    Pruebas en PokemonOptions

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,58 @@
+
+import { shallowMount } from '@vue/test-utils'
+import PokemonOptions from '@/components/PokemonOptions'
+import { pokemons } from '../mocks/pokemons.mock'
+
+describe('PokemonOptions component', () => {
+    
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount(PokemonOptions, {
+            props: {
+                pokemons
+            }
+        })
+    })
+
+    test('debe de hacer match con el snapshot', () => {
+        
+        expect(wrapper.html()).toMatchSnapshot()
+
+    })
+
+    test('debe de mostrar las 4 opciones correctamente', () => {
+        
+        // buscar todos los li
+        const liTags = wrapper.findAll('li')
+        expect(liTags.length).toBe(4)
+
+        // evaluar el nombre de cada uno
+        expect(liTags[0].text()).toBe('bulbasaur')
+        expect(liTags[1].text()).toBe('ivysaur')
+        expect(liTags[2].text()).toBe('venusaur')
+        expect(liTags[3].text()).toBe('charmander')
+
+    })
+
+    test('debe de emitir "selection" con sus parametros al hacer clic', () => {
+
+        const [li1, li2, li3, li4] = wrapper.findAll('li')
+
+        // clic en el elemento
+        li1.trigger('click')
+        li2.trigger('click')
+        li3.trigger('click')
+        li4.trigger('click')
+
+        // esperar que se emita n numero
+        expect(wrapper.emitted('selection').length ).toBe(4)
+        // evaluar el valor que emitio
+        expect(wrapper.emitted('selection')[0]).toEqual([1])
+        expect(wrapper.emitted('selection')[1]).toEqual([2])
+        expect(wrapper.emitted('selection')[2]).toEqual([3])
+        expect(wrapper.emitted('selection')[3]).toEqual([4])
+
+    })
+    
+})
