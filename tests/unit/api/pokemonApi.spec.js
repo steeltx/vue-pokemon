b08d2deeaf20bfa36502419768b0b43d32f96d80
steeltx <realsteel.gnu@gmail.com>
2021-08-22 15:02:30 -0500

    Prueba de baseURL con axios

---
+++ tests/unit/api/pokemonApi.spec.js
@@ -0,0 +1,14 @@
+import pokemonApi from '@/api/pokemonApi'
+
+describe('pokemonAPI', () => {
+    
+    test('axios debe de estar configurado con el api de pokemon', () => {
+
+        // revisar que la url de consulta este correcta
+        expect(pokemonApi.defaults.baseURL).toBe('https://pokeapi.co/api/v2/pokemon')
+
+    })
+    
+
+
+})
