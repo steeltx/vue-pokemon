bda4d2a6a85430783d7cebecdd7a48914b0e32fc
steeltx <realsteel.gnu@gmail.com>
2021-08-22 00:05:45 -0500

    Ajustando css de imagen

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -32,20 +32,18 @@ export default {
 </script>
 
 <style scoped>
-.pokemon-container {
-  height: 200px;
-}
-img {
-  height: 200px;
-  position: absolute;
-  right: 32%;
-  user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  -webkit-user-drag: none;
-  -webkit-user-select: none;
-}
-.hidden-pokemon {
-  filter: brightness(0);
-}
+  .pokemon-container {
+    height: 200px;
+  }
+  img {
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
 </style>
