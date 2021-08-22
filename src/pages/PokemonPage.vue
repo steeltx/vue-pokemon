209f3df7f7e9f1778bce7dce0e5fc8a2a66b7405
steeltx <realsteel.gnu@gmail.com>
2021-08-21 23:59:28 -0500

    Mostrando resultado

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,12 +9,19 @@
         <!-- IMG -->
         <PokemonPicture 
             :pokemonId="pokemon.id" 
-            :showPokemon="showPokemon"/>
+            :showPokemon="showPokemon"
+        />
 
         <!-- Opciones -->
         <PokemonOptions 
             :pokemons="pokemonArr" 
-            @selection="checkAnswer"/>
+            @selection="checkAnswer"
+        />
+
+        <template v-if="showAnswer">
+            <h2 class="fade-in">{{message}}</h2>
+            <button @click="newGame">Nuevo juego</button>
+        </template>
 
     </div>
    
@@ -36,7 +43,9 @@ export default {
         return{
            pokemonArr: [],
            pokemon: null,
-           showPokemon : false
+           showPokemon : false,
+           showAnswer : false,
+           message: ''
         }
     },
     methods:{
@@ -46,8 +55,21 @@ export default {
             this.pokemon = this.pokemonArr[rndInt]
         },
         // ejecutar funcion al ser emitida desde componente hijo
-        checkAnswer(pokemonId){
+        checkAnswer(selectecId){
             this.showPokemon = true
+            this.showAnswer = true
+            if(selectecId === this.pokemon.id){
+                this.message =  `Correcto, ${this.pokemon.name}`
+            }else{
+                this.message = `Oops, era ${this.pokemon.name}`
+            }
+        },
+        newGame(){
+            this.showPokemon = false
+            this.showAnswer = false
+            this.pokemonArr = []
+            this.pokemon = null
+            this.mixPokemonArray()
         }
     },
     mounted(){
